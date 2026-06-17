dcd3fa0792ab4fee64151e8d1bb07b7fc0986ada
AbdulrahmanMS-Eng <best5five1@gmail.com>
2026-06-18 01:40:51 +0300

    update project

--- src/components/rollc/assistant/Assistant.tsx
+++ src/components/rollc/assistant/Assistant.tsx
@@ -105,7 +105,7 @@ function ContactBlock() {
     <div className={styles.contactBlock}>
       <a href="tel:+966552055514" className={styles.contactLink}>
         <svg viewBox="0 0 24 24"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 11.5a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.62 1h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.1a16 16 0 0 0 6 6l.98-.98a2 2 0 0 1 2.1-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 21.78 15.5Z" /></svg>
-        <span>+966 55 205 5514</span>
+        <span><bdi dir="ltr">+966 55 205 5514</bdi></span>
       </a>
       <a href="mailto:customerservice@rollcksa.com" className={styles.contactLink}>
         <svg viewBox="0 0 24 24"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2Z" /><polyline points="22,6 12,13 2,6" /></svg>
